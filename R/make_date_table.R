f449ba06fde9a0acdf22a3174659e86626391869
Raymond Balise <balise@miami.edu>
2020-10-08 12:43:16 -0400

    add function shells

---
+++ R/make_date_table.R
@@ -0,0 +1,12 @@
+#' make_date_table
+#'
+#' @param tables List of table names
+#' @param los Length of study in days
+#'
+#' @return df
+#' @export
+#'
+#' @examples make_data_table(list(a, b), 23)
+make_data_table <- function(tables, los) {
+  
+}
