20ea8d65294914a0a38312fef74a19ceed7be986
gabrielodom <gabriel.odom@fiu.edu>
2022-02-01 12:19:26 -0500

    a stab at kNN imputation for missing visits (for Eissenberg et al. 1997). UNSTABLE

--- R/impute_missing_visits.R
+++ R/impute_missing_visits.R
@@ -58,10 +58,17 @@
 #'   impute_missing_visits(pattern2_char)
 #'   
 impute_missing_visits <- function(use_pattern,
-                                  method = c("locf", "locfD", "mode"),
+                                  method = c("locf", "locfD", "mode", "kNN"),
                                   missing_is = "o",
                                   mixed_is = "*",
                                   tiebreaker = "+",
+                                  k = 1,
+                                  knnWeights_num = c(
+                                    `o` = NA,
+                                    `+` = 1,
+                                    `*` = 0.5,
+                                    `-` = 0
+                                  ),
                                   quietly = FALSE) {
   # browser()
   
@@ -121,6 +128,15 @@ impute_missing_visits <- function(use_pattern,
         missing_is = missing_is,
         mixed_is = mixed_is,
         tiebreaker = tiebreaker
+      )
+    },
+    
+    kNN  = {
+      warning("kNN imputation in beta development. Expect bugs.", call. = FALSE)
+      .impute_kNN(
+        all_chars,
+        k = k,
+        weights_num = knnWeights_num
       )
     }
   )
@@ -190,3 +206,77 @@ impute_missing_visits <- function(use_pattern,
   
 }
 
+
+
+######  kNN  ##################################################################
+.impute_kNN <- function(x, k = 1, weights_num){
+  # browser()
+  
+  # The case_when() syntax works great here when we are coding interactively;
+  #   it's much more ugly when the LHS and RHS are programatically defined
+  origVals_char <- names(weights_num)
+  newVals_num   <- unname(weights_num)
+  visits_int    <- length(x)
+  
+  
+  # Recode to numeric
+  xNew <- x
+  for ( i in seq_along(origVals_char) ) {
+    xNew[x == origVals_char[i]] <- newVals_num[i]
+  }
+  x_num <- as.numeric(xNew)
+  
+  
+  # Boundary and Domain
+  domain_int <- seq.int(from = 1 + k, to = visits_int - k)
+  toImpute_idx <- which( is.na( x_num ) )
+  toImputeInDomain_idx <- toImpute_idx[toImpute_idx %in% domain_int]
+  
+  
+  # Imputation
+  xImpute_num <- x_num
+  for ( i in toImputeInDomain_idx ) {
+    
+    nbhd_idx <- seq.int(from = i - k, i + k)
+    deletedNbhd_idx <- nbhd_idx[nbhd_idx != i]
+    
+    # average in the window, allowing missings to trump other values (this will 
+    #   work for k > 1 only in cases where missing values are rare)
+    xImpute_num[i] <- mean( x_num[deletedNbhd_idx], na.rm = FALSE )
+    
+  }
+  
+  
+  # Recode back to character
+  numericWeights_num <- weights_num[!is.na(weights_num)]
+  xRound_num <- .round_to(xImpute_num, numericWeights_num)
+  xOut_char <- character(length = visits_int)
+  for ( i in seq_along(newVals_num) ) {
+    xOut_char[xRound_num == newVals_num[i]] <- origVals_char[i]
+  }
+  xOut_char[is.na(xRound_num)] <- "o"
+  
+  # Return
+  xOut_char
+  
+}
+
+.round_to <- function(num, values_num) {
+  
+  dist_mat <- outer(
+    X = num,
+    Y = values_num,
+    FUN = function(x, y) { abs(x - y) }
+  )
+  # which.min() returns integer(0) instead of NA for all NA values
+  mins_ls <- apply(X = dist_mat, MARGIN = 1, FUN = which.min, simplify = FALSE)
+  mins_ls[lengths(mins_ls) == 0] <- NA_integer_
+  values_num[unlist(mins_ls)]
+  
+}
+
+# # Test
+# impute_missing_visits(
+#   use_pattern = "++++*o-------+--+oo-o-o+o+",
+#   method = "kNN"
+# )
